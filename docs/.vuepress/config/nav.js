ad2d72d41ae71db4b551f51d003e4884172a312f
Ardient <ardien@126.com>
2020-07-23 00:45:46 +0800

    blog init

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,7 @@
+module.exports = [
+  {text: '首页', link: '/'},
+  {text: '分类', link: '/categories/'},
+  {text: '标签', link: '/tags/'},
+  {text: '关于', link: '/about/'},
+  {text: '归档', link: '/archives/'}
+]
