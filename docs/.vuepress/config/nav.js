e12297470beb1f11fc497df736b762f031b04640
Ardient <ardien@126.com>
2020-07-24 00:10:45 +0800

    更新文章

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,5 +1,18 @@
 module.exports = [
   {text: '首页', link: '/'},
+  {text: '分享美食',link: '/food/'},
+  {
+    text: '专栏',
+    link: '/column/',  //目录页，vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      
+        {text: ' Java基础', link: '/note/java/'},
+        {text: ' Netty', link: '/note/netty/'},
+        {text: ' Spring', link: '/note/spring/'},
+        {text: ' Spring Boot', link: '/note/springboot/'},
+      
+    ]
+  },
   {text: '分类', link: '/categories/'},
   {text: '标签', link: '/tags/'},
   {text: '归档', link: '/archives/'},
