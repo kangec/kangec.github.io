bf399835af8c29885b385fb6ed1c2c8c0baaf7f2
Ardient <ardien@126.com>
2020-07-25 22:48:26 +0800

    更新配置

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -7,9 +7,9 @@ module.exports = [
     link: '/column/',  //目录页，vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       
-        {text: ' Java基础', link: '/note/java/'},
-        {text: ' Netty', link: '/note/netty/'},
-        {text: ' Spring', link: '/note/spring/'},
+        {text: ' Java基础', link: '/pages/66f7b1/'},
+        {text: ' Netty', link: '/pages/943ab2/'},
+        {text: ' Spring', link: '/pages/958a5b/'},
         {text: ' Spring Boot', link: '/note/springboot/'},
       
     ]
